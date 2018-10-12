9021c5cca312805d0ce0047856bbcce08ddc9d08
Isaac Krementsov <isaackrementsov@gmail.com>
2018-10-12 12:54:57 -0400

    Command line arguments

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,15 +4,42 @@ import app.models.*;
 import advance.Server;
 public class App {
     public static void main(String[] args){
+        String root = "C:/users/isaac/documents/java/kokosole";
+        for(int i = 0; i < args.length; i++){
+            switch(args[i].charAt(0)){
+                case 'm': //"m-User,Trip,Location"
+                    String dbString = args[i].substring(2);
+                    if(dbString.equals("all")){
+                        User.migrate();
+                        Trip.migrate();
+                        Location.migrate();
+                        Activity.migrate();
+                    }else{
+                        String[] dbs = args[i].split(",");
+                        for(String db : dbs){
+                            switch(db){
+                                case "User":
+                                    User.migrate();
+                                    break;
+                                case "Trip":
+                                    Trip.migrate();
+                                    break;
+                                case "Location":
+                                    Location.migrate();
+                                    break;
+                                case "Activity":
+                                    Activity.migrate();
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+                case 'd': //"m-/your/directory"
+                    root = args[i].substring(2);
+                    break;
+            }
+        }
         Server app = new Server(9000, "C:/users/isaac/documents/java/kokosole");
-        if(args.length > 0){
-            if(args[0].equals("migrate")){
-                User.migrate();
-                Trip.migrate();
-                Location.migrate();
-                Activity.migrate();
-            }    
-        }    
         app.setViewDir("/views/");
         app.addController("/", new MainController());
         app.addController("/auth/:action", new AuthController());
